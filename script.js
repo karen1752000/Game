03030ca7d7f9403661c8bbf83cb23816b193a786
Karen Mitchell <karen1752000@yahoo.com>
2018-04-05 18:05:20 -0400

    styling added

--- script.js
+++ script.js
@@ -27,3 +27,112 @@ function hangman() {
 	var keyboard = document.getElementById("hangman-keyboard");
 	var playBtn = document.getElementById("hangman-play");
 	var messageContainer = document.getElementById("hangman-message");
+
+function renderCurrentWord() {
+	var wordContainer = document.getElementById("hangman-word");
+	wordContainer.innerHTML = ""; 
+
+	secretWord.forEach(function(char) {
+		var span = document.createElement("span");
+		span.className = "letter";
+		span.innerHTML = char;
+		wordContainer.appendChild(span);
+
+	});
+}
+
+function renderMessage(msg) {
+		messageContainer.innerHTML = msg;
+	}
+
+	function renderLives() {
+		var message = "You have"  + lives +  " lives left!";
+		if (lives <= 0) {
+			message = "Game over!";
+		}
+		renderMessage(message);
+	}
+
+	function revealLetter(letter) {
+		for (var i = 0; i < word.length; i++) {
+			if (letter == word[i]) {
+				secretWord[i] = word[i];
+			}
+		}
+		renderCurrentWord();
+	}
+
+	function buttonClick(e) {
+		e.preventDefault();
+
+		var btn = this;
+		var letter = btn.innerHTML; // Get element content
+		var notFound = -1;
+		var correct = word.indexOf(letter) != notFound; // Check if word contains letter
+		btn.disabled = true;
+
+		if (correct) {
+			revealLetter(letter);
+			if (secretWord.indexOf("_") == notFound) {
+				// Hidden char
+				renderMessage("You Win!");
+			}
+		} else {
+			lives -= 1;
+			renderLives();
+			if (lives <= 0) {
+				deactivateKeyBoard();
+			}
+		}
+	}
+
+	function renderKeyBoard() {
+		// Renders alphabetic keyboard
+		alphabet.forEach(function(letter) {
+			var alphabetBtn = document.createElement("button");
+			alphabetBtn.className = "abtn";
+			alphabetBtn.innerHTML = letter;
+			keyboard.appendChild(alphabetBtn);
+		});
+	}
+
+	function activateKeyBoard() {
+		var buttons = keyboard.getElementsByTagName("button");
+		for (var i = 0; i < buttons.length; i++) {
+			var element = buttons[i];
+			element.disabled = false;
+			element.addEventListener("click", buttonClick);
+		}
+	}
+
+	function deactivateKeyBoard() {
+		var buttons = keyboard.getElementsByTagName("button");
+		for (var i = 0; i < buttons.length; i++) {
+			var element = buttons[i];
+			element.removeEventListener("click", buttonClick);
+		}
+	}
+
+	function play() {
+		lives = 10;
+		word = getRandomWord();
+		secretWord = word.split("").map(function() {
+			return "_";
+		});
+
+		activateKeyBoard();
+		renderLives();
+		renderCurrentWord();
+	}
+
+	// Start hangman
+	function init() {
+		playBtn.addEventListener("click", play);
+		renderKeyBoard();
+		play();
+	}
+
+	init();
+}
+
+hangman();
